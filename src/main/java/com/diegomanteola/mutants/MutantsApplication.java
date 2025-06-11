600de47195e665acafa2186ed9aefecf09d824fa
DiegoManteola <Diego_Manteola@HotMail.com>
2025-06-11 13:51:26 -0300

    Compatibilización de SpringBoot con .env para simplificación de parametrización

--- src/main/java/com/diegomanteola/mutants/MutantsApplication.java
+++ src/main/java/com/diegomanteola/mutants/MutantsApplication.java
@@ -3,11 +3,14 @@ package com.diegomanteola.mutants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableCaching
 public class MutantsApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(MutantsApplication.class, args);
     }
 }
